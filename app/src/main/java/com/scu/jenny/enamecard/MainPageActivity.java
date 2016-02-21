f80e2ba70f55340fbdf323b6c2d6f4bee973bc8e
Manhong Ren <manhongren@hotmail.com>
2016-02-21 10:18:09 -0800

    add tab bar at bottom

--- app/src/main/java/com/scu/jenny/enamecard/MainPageActivity.java
+++ app/src/main/java/com/scu/jenny/enamecard/MainPageActivity.java
@@ -1,21 +1,27 @@
 package com.scu.jenny.enamecard;
 
 import android.app.ActionBar;
+import android.app.Fragment;
+import android.app.TabActivity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TabHost;
 
-public class MainPageActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MainPageActivity extends TabActivity implements TabHost.TabContentFactory {
+    List<Fragment> tabFragmentList = new ArrayList<Fragment>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -25,8 +31,19 @@ public class MainPageActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        TabHost tabHost = getTabHost();
+        tabHost.addTab(tabHost.newTabSpec("My profile").setIndicator("eBay").setContent(this));
+        tabHost.addTab(tabHost.newTabSpec("cards").setIndicator("Flickr").setContent(this));
+  ;
+
+//        setupUI();
 
     }
 
 
+    @Override
+    public View createTabContent(String tag) {
+        //
+        return null;
+    }
 }
