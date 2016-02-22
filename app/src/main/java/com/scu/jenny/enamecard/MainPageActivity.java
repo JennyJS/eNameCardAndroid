63a8d1be422430f94b7b76295a5e0bebace89e20
Manhong Ren <manhongren@hotmail.com>
2016-02-21 16:22:17 -0800

    Use log in activity as entry point

--- app/src/main/java/com/scu/jenny/enamecard/MainPageActivity.java
+++ app/src/main/java/com/scu/jenny/enamecard/MainPageActivity.java
@@ -23,19 +23,6 @@ public class MainPageActivity extends TabActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-
-
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
 
         // create the TabHost that will contain the Tabs
         TabHost tabHost = (TabHost)findViewById(android.R.id.tabhost);
