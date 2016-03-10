b609697e6e8bc466969a7cc80b6ce5b806d8c277
Manhong Ren <manhongren@hotmail.com>
2016-03-09 18:15:17 -0800

    add twitter, leave for testing

---
+++ app/src/main/java/com/scu/jenny/enamecard/TwitterActivity.java
@@ -0,0 +1,75 @@
+package com.scu.jenny.enamecard;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+import com.twitter.sdk.android.tweetcomposer.TweetComposer;
+
+
+/**
+ * Created by jenny on 3/9/16.
+ */
+public class TwitterActivity extends Activity {
+    private TwitterLoginButton twitterButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.twitter_login_btn);
+        setUpViews();
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        twitterButton.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void setUpViews() {
+        setUpTwitterButton();
+    }
+
+    private void setUpTwitterButton() {
+        twitterButton = (TwitterLoginButton) findViewById(R.id.twitter_button);
+        twitterButton.setCallback(new Callback() {
+            @Override
+            public void success(Result result) {
+                Toast.makeText(getApplicationContext(),
+                        getResources().getString(R.string.app_name),
+                        Toast.LENGTH_SHORT).show();
+
+                setUpViewsForTweetComposer();
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                Toast.makeText(getApplicationContext(),
+                        getResources().getString(R.string.app_name),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void setUpViewsForTweetComposer() {
+        TweetComposer.Builder builder = new TweetComposer.Builder(this)
+                .text("Just setting up my Fabric!");
+        builder.show();
+    }
+}
---
+++ app/src/main/java/com/scu/jenny/enamecard/TwitterInit.java
@@ -0,0 +1,31 @@
+package com.scu.jenny.enamecard;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+
+import io.fabric.sdk.android.Fabric;
+
+/**
+ * Created by jenny on 3/9/16.
+ */
+public class TwitterInit extends Activity {
+    private static final String TWITTER_KEY = "WDuhNUL7cEztJEKuPnqfBDV2K";
+    private static final String TWITTER_SECRET = "5vVNEtG2UvJBOgVbIZ6xBzb6sgwZefRMG7332JLPyeJVwGxqz4";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
+        startLoginActivity();
+    }
+
+    private void startLoginActivity() {
+        startActivity(new Intent(this, TwitterActivity.class));
+    }
+}
